1f29d840dda1f618f2fc8c62fb71c9990b9a4691
TESLA <57leonardo@gmail.com>
2016-02-17 17:36:08 -0500

    not wokring

--- c/draw.c
+++ c/draw.c
@@ -60,23 +60,4 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   }
   
-  //OCTANT VIII
-  m = (y1-y0)/(x1-x0); 
-  if ((m >= -1 && m <= 0)){
-    x = x0; y = y0;
-    A = y1-y0; B = -(x1-x0);
-    d = 2*A + B;
-    while(x <= x1){
-      plot(s, c, x, y);
-      if (d<0){
-	y+=1;
-	d+=2*B;
-      }
-      x+=1;
-      d+=2*A;
-    }
-  }
-
-
-
 }
