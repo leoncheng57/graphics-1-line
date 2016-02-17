9c3ea2faf33de46e52c6d93485d8e11ff3563d90
TESLA <57leonardo@gmail.com>
2016-02-17 17:30:37 -0500

    starting octant 8

--- c/draw.c
+++ c/draw.c
@@ -27,33 +27,56 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     
 
   //OCTANT I
-  m = (y1-y0)/(x1-x0); if (!(m <= 1 && m >= 0)) return;
-  x = x0; y = y0;
-  A = y1-y0; B = -(x1-x0);
-  d = 2*A + B;
-  while(x <= x1){
-    plot(s, c, x, y);
-    if (d>0){
-      y+=1;
-      d+=2*B;
-    }
-    x+=1;
-    d+=2*A;
-  }
-  
-  //OCTANT II
-  m = (y1-y0)/(x1-x0); if (!(m >= 1)) return;
-  x = x0; y = y0;
-  A = y1-y0; B = -(x1-x0);
-  d = A+2*B;
-  while(y <= y1){
-    plot(s,c,x,y);
-    if (d<0){
+  m = (y1-y0)/(x1-x0); 
+  if ((m <= 1 && m >= 0)){
+    x = x0; y = y0;
+    A = y1-y0; B = -(x1-x0);
+    d = 2*A + B;
+    while(x <= x1){
+      plot(s, c, x, y);
+      if (d>0){
+	y+=1;
+	d+=2*B;
+      }
       x+=1;
       d+=2*A;
     }
-    y+=1;
-    d+=2*B;
   }
+  
+  //OCTANT II
+  m = (y1-y0)/(x1-x0); 
+  if ((m >= 1)){
+    x = x0; y = y0;
+    A = y1-y0; B = -(x1-x0);
+    d = A+2*B;
+    while(y <= y1){
+      plot(s,c,x,y);
+      if (d<0){
+	x+=1;
+	d+=2*A;
+      }
+      y+=1;
+      d+=2*B;
+    }
+  }
+  
+  //OCTANT VIII
+  m = (y1-y0)/(x1-x0); 
+  if ((m >= -1 && m <= 0)){
+    x = x0; y = y0;
+    A = y1-y0; B = -(x1-x0);
+    d = 2*A + B;
+    while(x <= x1){
+      plot(s, c, x, y);
+      if (d<0){
+	y+=1;
+	d+=2*B;
+      }
+      x+=1;
+      d+=2*A;
+    }
+  }
+
+
 
 }
