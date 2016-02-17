dff68f6755f79be457f1c9b2661fb22afae10d24
TESLA <57leonardo@gmail.com>
2016-02-17 17:13:14 -0500

    workin on octant 1

--- c/draw.c
+++ c/draw.c
@@ -7,28 +7,48 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  // *_Algorithm_* (Octant 2)
-  //   (x_o, y_o) -> (x_1, y_1)
-  //   x=x_o, y=y_o
-  //   A = y_1 - y_o, B = -(x_1 - x_o)
-  //   d = A + 2B
-  //   while(y \leq y_1)
-  //     plot (x, y)
-  //     if (d<0)
-  //       x+=1
-  //       d+=2A
-  //     y+=1
-  //     d+=2B
-  if (!(x1>x0))
-    return;
-  if (!((y1-y0)/(x1-x0)>=1))
-    return;
+/*************************************/
+/* *_Algorithm_* (Octant 1)	     */
+/*   (x_o, y_o) -> (x_1, y_1)  	     */
+/*   x=x_o, y=y_o		     */
+/*   A = y_1 - y_o, B = -(x_1 - x_o) */
+/*   d = 2A + B			     */
+/*   while(x \leq x_1)		     */
+/*     plot (x, y)		     */
+/*     if (d>0)			     */
+/*       y+=1			     */
+/*       d+=2B			     */
+/*     x+=1			     */
+/*     d+=2A			     */
+/*************************************/
+  
+  int x, y, A, B, d, m;  
+  
+  if (!(x1>x0)) return;
+
+  //OCTANT I
+  m = (y1-y0)/(x1-x0); if (!(m <= 1 && m >= 0)) return;
+  x = x0; y = y0;
+  A = y1-y0; B = -(x1-x0);
+  d = 2*A + B;
+  while(x <= x1){
+    plot(s, c, x, y);
+    if (d>0){
+      y+=1;
+      d+=2*B;
+    }
+    x+=1;
+    d+=2*A;
+  }
+  
   //OCTANT II
-  int x = x0;
-  int y = y0;
-  int A = y1-y0;
-  int B = -(x1-x0);
-  int d = A+2*B;
+  m = (y1-y0)/(x1-x0); 
+  if (!(m >= 1)) return;
+  x = x0;
+  y = y0;
+  A = y1-y0;
+  B = -(x1-x0);
+  d = A+2*B;
   while(y <= y1){
     plot(s,c,x,y);
     if (d<0){
